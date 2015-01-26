e2e77276d3d265f697fb6a08414b43a186e71103
Lukas Steiblys <imbusy@imbusy.org>
2015-01-25 21:42:53 -0800

    command-line tool working

--- src/cut/cpp/cut.cpp
+++ src/cut/cpp/cut.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include <libcut.hpp>
 
 int main(int argc, char *argv[]) {
+   // arguments to parse out
    bool bytes = false;
    bool fields = false;
-   int curParam = 1;
+   bool complement = false;
+   std::string list = "";
+   char delimiter = '\t';
+   std::string input = "";
+   
+   // parse all the arguments
+   for(int param = 1; param < argc; param++) {
+      auto arg = std::string(argv[param]);
+      std::cout<<arg<<' '<<param<<' '<<argc<<'\n';
+      if(arg == "-b" && param+1 < argc) {
+         bytes = true;
+         list = std::string(argv[param+1]);
+         param++;
+      } else if(arg == "-f" && param+1 < argc) {
+         fields = true;
+         list = std::string(argv[param+1]);
+         param++;
+      } else if(arg == "--complement") {
+         complement = true;
+      } else if(arg == "-d" && param+1 < argc) {
+         std::cout<<"D\n";
+         delimiter = argv[param+1][0];
+         param++;
+      } else {
+         input = arg;
+         break;
+      }
+   }
+   if((bytes && fields) || (!bytes && !fields)) {
+      std::cout<<"Either bytes or fields should be selected."<<std::endl;
+      return -1;
+   }
+   
+   // process the input file
+   std::ifstream file(input, std::ifstream::in);
+   std::string line;
+   while(getline(file, line)) {
+      if(bytes) {
+         std::cout<<cut::bytes(line, list, complement)<<std::endl;
+      } else if(fields) {
+         std::cout<<cut::fields(line, list, delimiter, complement)<<std::endl;
+      }
+   }
+   
    return 0;
 }
